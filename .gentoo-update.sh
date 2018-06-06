eaf24e25bcb7374c6ec2597d41003720725897b1
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2018-06-06 09:44:07 +0700

    изменения .emacs, вывел окно компиляции в отдельное окно

--- .gentoo-update.sh
+++ .gentoo-update.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 
+
+
+#https://wiki.gentoo.org/wiki/Upgrading_Gentoo/ru#.D0.9E.D0.B1.D0.BD.D0.BE.D0.B2.D0.BB.D0.B5.D0.BD.D0.B8.D0.B5_.D1.81.D1.82.D0.B0.D1.80.D1.8B.D1.85_.D1.81.D0.B8.D1.81.D1.82.D0.B5.D0.BC
+
+
+#mv /usr/portage /usr/portage.latest
+#tar xjpf /path/to/portage-20090720.tar.bz2 -C /usr
+#emerge -u portage
+#gpg --verify portage-20090720.tar.bz2.gpgsig portage-20090720.tar.bz2
