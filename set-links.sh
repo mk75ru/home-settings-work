4fb008c1202c0512b6a3ef4d1c24d4fef7c7b46a
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2018-09-03 16:47:45 +0700

    удалил делание ссылки на gitignore

--- set-links.sh
+++ set-links.sh
@@ -3,7 +3,6 @@
 ln -s -T  ~/home-settings-work/acces-to-syncthing-mknode.sh  ../acces-to-syncthing-mknode.sh
 ln -s -T  ~/home-settings-work/.gentoo-update.sh  ../.gentoo-update.sh
 ln -s -T  ~/home-settings-work/.git-completion.bash  ../.git-completion.bash
-ln -s -T  ~/home-settings-work/.gitignore  ../.gitignore
 ln -s -T  ~/home-settings-work/.git-remove-submodule  ../.git-remove-submodule
 ln -s -T  ~/home-settings-work/.xinitrc  ../.xinitrc
 ln -s -T  ~/home-settings-work/org_work  ../org
