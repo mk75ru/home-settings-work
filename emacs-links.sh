ae415c1416d9453dc230b048e5890a59a40bfe1e
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2018-09-03 16:26:41 +0700

    поправил emacs-links.sh

--- emacs-links.sh
+++ emacs-links.sh
@@ -8,7 +8,7 @@ ln -s -T  ~/home-settings-work/.git-completion.bash  ../.git-completion.bash
 ln -s -T  ~/home-settings-work/.gitignore  ../.gitignore
 ln -s -T  ~/home-settings-work/.git-remove-submodule  ../.git-remove-submodule
 ln -s -T  ~/home-settings-work/.xinitrc  ../.xinitrc
-ln -s -T  ~/home-settings-work/emacs_work  ../.emacs
+ln -s -T  ~/home-settings-work/.emacs_work  ../.emacs
 
 
 
