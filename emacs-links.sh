e1520645cd917cacd25bfa8e3c0ab51640e5316c
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2018-09-03 16:24:58 +0700

    переименовал .emacs

--- emacs-links.sh
+++ emacs-links.sh
@@ -2,12 +2,13 @@
 
 
 ln -s -T ~/.emacs_work.d  ../.emacs.d
-ln -s -T  ~/home-settings-mk/acces-to-syncthing-mknode.sh  ../acces-to-syncthing-mknode.sh
-ln -s -T  ~/home-settings-mk/.gentoo-update.sh  ../.gentoo-update.sh
-ln -s -T  ~/home-settings-mk/.git-completion.bash  ../.git-completion.bash
-ln -s -T  ~/home-settings-mk/.gitignore  ../.gitignore
-ln -s -T  ~/home-settings-mk/.git-remove-submodule  ../.git-remove-submodule
-ln -s -T  ~/home-settings-mk/.xinitrc  ../.xinitrc
-ln -s -T  ~/home-settings-mk/emacs_work  ../.emacs
+ln -s -T  ~/home-settings-work/acces-to-syncthing-mknode.sh  ../acces-to-syncthing-mknode.sh
+ln -s -T  ~/home-settings-work/.gentoo-update.sh  ../.gentoo-update.sh
+ln -s -T  ~/home-settings-work/.git-completion.bash  ../.git-completion.bash
+ln -s -T  ~/home-settings-work/.gitignore  ../.gitignore
+ln -s -T  ~/home-settings-work/.git-remove-submodule  ../.git-remove-submodule
+ln -s -T  ~/home-settings-work/.xinitrc  ../.xinitrc
+ln -s -T  ~/home-settings-work/emacs_work  ../.emacs
+
 
 
