be47fda1d972046d5cd6ddba8de323ec90b3cb32
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2018-09-03 16:42:56 +0700

    разделил links и emacs-links

--- emacs-links.sh
+++ emacs-links.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
+rm ../.emacs
+rm ../.emacs.el
+ln -s -T  ~/home-settings-work/.emacs_work  ../.emacs
 
 ln -s -T ~/.emacs_work.d  ../.emacs.d
-ln -s -T  ~/home-settings-work/acces-to-syncthing-mknode.sh  ../acces-to-syncthing-mknode.sh
-ln -s -T  ~/home-settings-work/.gentoo-update.sh  ../.gentoo-update.sh
-ln -s -T  ~/home-settings-work/.git-completion.bash  ../.git-completion.bash
-ln -s -T  ~/home-settings-work/.gitignore  ../.gitignore
-ln -s -T  ~/home-settings-work/.git-remove-submodule  ../.git-remove-submodule
-ln -s -T  ~/home-settings-work/.xinitrc  ../.xinitrc
-ln -s -T  ~/home-settings-work/.emacs_work  ../.emacs
-ln -s -T  ~/home-settings-work/org_work  ../org
 
 
 
